ef469c869b074d860f469fdeb0acef6ad563ff96
viruj <virujthakur01@gmail.com>
2023-08-30 19:50:08 +0530

    Solution to Q9 Arrival of the general 144A

---
+++ Q9_A_Arrival_of_the_General.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define mod 998244353
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define INF (1LL << 60)
+#define NINF -INF
+#define endl "\n"
+
+
+void solve()
+{
+    int n;
+    cin>> n;
+
+    vector<int> soldiers(n);
+
+    for(int i=0; i<n;i++)
+    cin>> soldiers[i];
+
+    int mx= max_element(soldiers.begin(), soldiers.end())- soldiers.begin();
+    int count=0;
+    while(mx>0)
+    {
+        swap(soldiers[mx], soldiers[mx-1]);
+        mx--;
+        count++;
+    }
+
+    // for(int i=0; i<n;i++)
+    // cout<<soldiers[i]<<" ";
+    // cout<<endl;
+
+    reverse(soldiers.begin(), soldiers.end());
+
+    int mn= min_element(soldiers.begin(), soldiers.end())- soldiers.begin();
+    // cout<<mn<<endl;
+
+    cout<<count + mn<<endl;
+    
+
+}
+
+int main()
+{
+    fast_io;
+    int t;
+    t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
