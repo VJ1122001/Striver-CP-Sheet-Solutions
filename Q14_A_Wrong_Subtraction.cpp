1bb2989c4f83789f40c647537fbaf4657a096ebd
viruj <virujthakur01@gmail.com>
2023-09-01 20:22:26 +0530

    Solution to Q14 Wrong Subtraction 977A

---
+++ Q14_A_Wrong_Subtraction.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define mod 998244353
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define INF (1LL << 60)
+#define NINF -INF
+#define endl "\n"
+
+
+void solve()
+{
+    long long n;
+    cin>> n;
+
+    int k;
+    cin>> k;
+
+    while(k--)
+    {
+        if((n%10) > 0)
+            n--;
+        else
+            n/=10;
+    }
+
+    cout<<n<<endl;
+}
+
+int main()
+{
+    fast_io;
+    int t;
+    t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
