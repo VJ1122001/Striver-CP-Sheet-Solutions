6336a30d8fcc69719b34f1d2b7b7306478754ac8
viruj <virujthakur01@gmail.com>
2023-09-06 19:24:10 +0530

    Solution to Q17 A boy or girl 236A

---
+++ Q17_A_Boy_or_Girl.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define mod 998244353
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define INF (1LL << 60)
+#define NINF -INF
+#define endl "\n"
+
+
+void solve()
+{
+    string s;
+    cin>> s;
+
+    unordered_set<int> se;
+    for(auto c: s)
+        se.insert(c);
+
+    cout<<(se.size()%2==1? "IGNORE HIM!" : "CHAT WITH HER!")<<endl;
+}
+
+int main()
+{
+    fast_io;
+    int t;
+    t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
