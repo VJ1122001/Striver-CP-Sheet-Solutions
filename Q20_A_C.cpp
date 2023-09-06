3ed7a95c7f862d1a5e17783577fcc2334d95576d
viruj <virujthakur01@gmail.com>
2023-09-06 19:26:15 +0530

    Solution to Q20 C+= 1368A

---
+++ Q20_A_C.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define mod 998244353
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define INF (1LL << 60)
+#define NINF -INF
+#define endl "\n"
+
+
+void solve()
+{
+    long long a,b,n;
+    cin>> a>> b>> n;
+
+    int count=0;
+    while(a<=n && b<=n)
+    {
+        if(a<b)
+        a+=b;
+        else
+        b+=a;
+        count++;
+        // cout<<a<<" "<<b<<endl;
+    }
+
+    cout<<count<<endl;
+}
+
+int main()
+{
+    fast_io;
+    int t;
+    t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
